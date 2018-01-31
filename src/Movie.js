2d5bed3f53859aa164fdeacaff989dc78e90cdd4
woosuk-oh <gta2v@naver.com>
2018-01-31 19:44:29 +0900

    Dumb Component였던 funtional Component들은 ES6 Arrow Func 사용.

--- src/Movie.js
+++ src/Movie.js
@@ -4,33 +4,31 @@ import PropTypes from 'prop-types';
 import './Movie.css';
 //import MoviePoster from './MoviePoster';
 
-function Movie({title, poster}){
-    return (
-        <div>
-            <MoviePoster poster={poster}/>
-            <h1> {title} </h1>
-        </div>
-    )
-}
+const Movie = ({title, poster}) => {
+  return (
+    <div>
+      <MoviePoster poster={poster}/>
+      <h1> {title} </h1>
+    </div>
+  );
+};
 
-function MoviePoster({poster}){
-    return(
-        <img src={poster} alt="movie poster"/>
-    )
-}
-
-
-MoviePoster.propTypes= {
-    poster: PropTypes.string.isRequired
-}
-
-
-Movie.propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+const MoviePoster = ({poster}) => {
+  return (
+    <img src={poster} alt="movie poster"/>
+  );
 };
 
 
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
+};
+
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
 
 
 Movie.defaultProps = {};
